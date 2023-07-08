c6b49db350094f030f0e03f7e5e7becadef6ee09
이명재 <arkk20000@gmail.com>
2023-07-08 14:11:39 +0900

    리덕스 떵크 맛보기

---
+++ redux-thunk-project/src/features/posts/PostsList.js
@@ -0,0 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postsSlice";
+
+import { useEffect } from "react";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
+
+  let content;
+
+  if (postsStatus === "loading") {
+    content = <p>Loading...</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+
+    content = orderedPosts.map((post, idx) => (
+      <PostsExcerpt key={idx} post={post} />
+    ));
+    console.log(orderedPosts[0].reactions);
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
