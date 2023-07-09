d193348c354f350d881d0e610fcdbfd00bfcd729
이명재 <arkk20000@gmail.com>
2023-07-09 23:08:15 +0900

    리덕스 떵크로 CRUD 해보기

---
+++ project2/src/features/posts/PostsList.js
@@ -0,0 +1,35 @@
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  let content;
+
+  if (postsStatus === "loading") {
+    content = <p>Loading...</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+
+    content = orderedPosts.map((post, idx) => (
+      <PostsExcerpt key={idx} post={post} />
+    ));
+    console.log(orderedPosts[0].reactions);
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
